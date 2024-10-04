f78e3bcb85d52fc9f40fc207fbc1e9d90d5f33a3
vaibhav9852 <vaibhav.s@webkorps.com>
2024-10-04 19:19:36 +0530

    add loader

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
  
 
  const container = document.querySelector('.container');
-
+ 
  async function fetchPhoto(numOfImg = 5 ){
     try{
+        let loader = document.querySelector('.loader')
+     loader.style.visibility = "visible";
     let response = await fetch('https://api.unsplash.com/photos/?client_id=kM5--yERRF3QC3ywYJyi2RgQwRqWhDVxhs4f7lrzkys&count=${countVal}')
     let data = await response.json()
     console.log('data..',data) 
@@ -13,12 +15,20 @@
         img.src= `${item.links.download}`
         container.appendChild(img)
     })
-}catch(err){
+
+    // if(data?.length){
+    //     let loader = document.querySelector('.loader')
+    //     loader.remove();
+    // }
+}catch(err){ 
 
     const h2 = document.createElement('h2')
     h2.appendChild(document.createTextNode('Somehing Wrong !'))
     container.appendChild(h2)
 
+}finally{
+    let loader = document.querySelector('.loader')
+     loader.style.display = "none";
 }
     
  }
