31bbbda98cb93a8d4afc761d0f9adae4d2ee6644
vaibhav9852 <vaibhav.s@webkorps.com>
2024-10-12 18:51:56 +0530

    fix api

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
- 
+ const clientId = `kM5--yERRF3QC3ywYJyi2RgQwRqWhDVxhs4f7lrzkys`
+  const URL = `https://api.unsplash.com/photos` 
 
  const container = document.querySelector('.container');
  
- async function fetchPhoto(numOfImg = 5 ){
+ async function fetchPhoto(countVal = 5 ){
     try{
         let loader = document.querySelector('.loader')
      loader.style.visibility = "visible";
-    let response = await fetch('https://api.unsplash.com/photos/?client_id=kM5--yERRF3QC3ywYJyi2RgQwRqWhDVxhs4f7lrzkys&count=${countVal}')
+    let response = await fetch(`${URL}/?client_id=${clientId}&count=${countVal}`)
     let data = await response.json()
-    console.log('data..',data) 
-
     data.map((item) =>{
         const img = document.createElement('img')
         img.src= `${item.links.download}`
+        img.alt = `Image`
         container.appendChild(img)
     })
 
-    // if(data?.length){
-    //     let loader = document.querySelector('.loader')
-    //     loader.remove();
-    // }
-}catch(err){ 
+}catch(err){  
 
     const h2 = document.createElement('h2')
-    h2.appendChild(document.createTextNode('Somehing Wrong !'))
-    container.appendChild(h2)
+    h2.appendChild(document.createTextNode('Something wrong while api call !'))
+    document.querySelector('.container').appendChild(h2)
 
 }finally{
     let loader = document.querySelector('.loader')
@@ -32,13 +28,24 @@
 }
     
  }
+
  fetchPhoto()
 
  document.addEventListener('scroll',()=>{
      if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        console.log('total',window.scrollY + window.innerHeight)
         fetchPhoto()
      }
  })
 
+
+ //  The read-only scrollY property of the Window interface returns the number of pixels by which
+ //   the document is currently scrolled vertically.
+
+ // The value of innerHeight is taken from the height of the window's layout viewport. 
+ // The width can be obtained using the innerWidth property.
+
+ // The Element.scrollHeight read-only property is a measurement of the height of an element's content,
+ //  including content not visible on the screen due to overflow.
 
 
